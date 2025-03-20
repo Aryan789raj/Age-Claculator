288d27de81e0ff45daad9dde154b5ac7262acb50
Aryan789raj <giriaryan2222@gmail.com>
2025-03-21 02:18:37 +0530

    WOO

---
+++ age.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+// #include<.h>
+using namespace std;
+int main(){
+    cout << "----------------------------------------------------------" <<endl;
+    cout << "             Welcome to the age calculator" <<endl;
+    cout << "----------------------------------------------------------" ;
+    cout <<endl <<endl <<endl;
+    
+    int CurrentDate;
+    int CurrentMonth;
+    int CurrentYear;
+
+    int BirthDate;
+    int BirthMonth;
+    int BirthYear;
+    
+    cout <<"Enter your date of birth in format DD MM YY : " << endl;
+    cout << "DD : ";
+    cin >> BirthDate;
+    cout << "MM : ";
+    cin >> BirthMonth;
+    cout << "YY : " ;
+    cin >> BirthYear;
+    cout << endl;
+
+    cout<<"Enter current date in format DD MM YY : " << endl;
+
+    cout << "DD : ";
+    cin >> CurrentDate;
+    cout << "MM : ";
+    cin >> CurrentMonth;
+    cout << "YY : " ;
+    cin >> CurrentYear;
+    cout << endl;
+
+
+    if (CurrentDate < BirthDate){
+        CurrentMonth = CurrentMonth - 1;
+        CurrentDate = CurrentDate + 30;
+    }
+
+    if (CurrentMonth < BirthMonth){
+        CurrentYear = CurrentYear - 1;
+        CurrentMonth = CurrentMonth + 12;
+    }
+    
+    cout<<"Your age is : " << CurrentYear - BirthYear << " Years   " ; 
+    cout << CurrentMonth - BirthMonth << " Months   " << CurrentDate-BirthDate << " Days   " << endl ;
+
+}
